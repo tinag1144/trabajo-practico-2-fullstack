174a15d586f97f7d065fb8a55882eea90a3abdba
unknown <sogazelaznog@gmail.com>
2026-05-02 14:27:31 -0300

    creo hook personalizado para usar contexto

--- frontend/src/context/useRoutineContext.jsx
+++ frontend/src/context/useRoutineContext.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import RoutineContext from "./RoutineContext";
 
+// hago esto para no repetir useContext en todos lados
 export const useRoutineContext = () => {
   return useContext(RoutineContext);
 };
